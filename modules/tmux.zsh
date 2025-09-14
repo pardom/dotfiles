c79b98d4ec4d475ec757b3b02ad75618be349cb6
Michael Pardo <mpardo@netflix.com>
2025-09-14 10:27:35 -0400

    Don't check in tmux plugins.

--- modules/tmux.zsh
+++ modules/tmux.zsh
@@ -5,6 +5,13 @@ if ! command -v tmux >/dev/null; then
   brew install tmux
 fi
 
+export TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+# Ensure TPM is installed
+if [ ! -d "$TPM_DIR" ]; then
+  git clone https://github.com/tmux-plugins/tpm $TPM_DIR
+fi
+
 alias tml='tmux list-sessions'
 alias tma='tmux new -As $(pwd | sed "s/.*\///g")'
 alias tmk='tmux list-sessions -F "#S" | fzf --height ~100% | xargs tmux kill-session -t '
