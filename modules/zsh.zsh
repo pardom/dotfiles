3b3bdb1cba9830015ad13d9086edefee3d1c4b01
Michael Pardo <mpardo@netflix.com>
2025-09-17 14:49:47 -0400

    Add fzf-tab.

--- modules/zsh.zsh
+++ modules/zsh.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
 # Case-insensitive completion
-zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
+# zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
+
+# To make fzf-tab follow FZF_DEFAULT_OPTS.
+# NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
 
